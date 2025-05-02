52f165db6988b96cdaa08ac1da2d01976daec907
uerkw <38047633+uerkw@users.noreply.github.com>
2025-05-02 02:51:36 -0400

    whack wrangler into submission

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import Unfonts from "unplugin-fonts/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
     plugins: [
+        cloudflare({ viteEnvironment: { name: "ssr" } }),
         tailwindcss(),
         reactRouter(),
         tsconfigPaths(),
